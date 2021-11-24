c0b3ce35c4471f4bd4ba922a7d76db91fd5072f1
GuiQRSM <endbeob15@gmail.com>
2021-11-24 16:22:36 -0300

    Implement inside Container(), after the child: a SingleChildScrollView() and inside this same class put Column() to activate a scroll behavior on the body

--- lib/MaterialLayout.dart
+++ lib/MaterialLayout.dart
@@ -27,76 +27,89 @@ class _MaterialLayoutState extends State<MaterialLayout> {
       ),
       body: Container(
         padding: EdgeInsets.all(32),
-       child: Column(
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(bottom: 32),
-              child: Image.asset(
-                  "logo/newlogocar.png"
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(bottom: 10),
-              child: Text(
-                "Saiba qual a melhor opção para abastecimento do seu carro",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w500,
-                  color: primeColor,
-                ),),
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: "Preço do Álcool, ex: 1.39",
-                labelStyle: TextStyle(
-                  fontSize: 18,
-                  color: primeColor,
-                ),
-                contentPadding: EdgeInsets.fromLTRB(30, 16, 30, 16),
-              ),
-              style: TextStyle(
-                fontSize: 15,
-                color: primeColor,
-              ),
-              controller: _controllerAlcool,
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: "Preço da Gasolina, ex: 1.39",
-                labelStyle: TextStyle(
-                  fontSize: 18,
-                  color: primeColor,
-                ),
-                contentPadding: EdgeInsets.fromLTRB(30, 16, 30, 16),
-                focusColor: primeColor,
-              ),
-              style: TextStyle(
-                  fontSize: 15,
-                  color: primeColor
-              ),
-              controller: _controllerGasolina,
-            ),
-            Padding(
-                padding: EdgeInsets.only(top: 13),
-              child: RaisedButton(
-                onPressed: () {
-                  print("Dado coletado Álcool: " + _controllerAlcool.text);
-                  print("Dado coletado Gasolina " + _controllerGasolina.text);
-                },
-                color: primeColor,
-                child: Text(
-                  "Calcular",
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),),
-                padding: EdgeInsets.only(right: 26, left: 26),
-              ),
-            ),
-          ],
-        ),
+       child: SingleChildScrollView(
+         child: Column(
+           crossAxisAlignment: CrossAxisAlignment.stretch,
+           children: <Widget>[
+             Padding(
+               padding: EdgeInsets.only(bottom: 32),
+               child: Image.asset(
+                   "logo/newlogocar.png"
+               ),
+             ),
+             Padding(
+               padding: EdgeInsets.only(bottom: 10),
+               child: Text(
+                 "Saiba qual a melhor opção para abastecimento do seu carro",
+                 textAlign: TextAlign.center,
+                 style: TextStyle(
+                   fontSize: 20,
+                   fontWeight: FontWeight.w500,
+                   color: primeColor,
+                 ),),
+             ),
+             TextField(
+               keyboardType: TextInputType.number,
+               decoration: InputDecoration(
+                 labelText: "Preço do Álcool, ex: 1.39",
+                 labelStyle: TextStyle(
+                   fontSize: 18,
+                   color: primeColor,
+                 ),
+                 contentPadding: EdgeInsets.fromLTRB(30, 16, 30, 16),
+               ),
+               style: TextStyle(
+                 fontSize: 15,
+                 color: primeColor,
+               ),
+               controller: _controllerAlcool,
+             ),
+             TextField(
+               keyboardType: TextInputType.number,
+               decoration: InputDecoration(
+                 labelText: "Preço da Gasolina, ex: 1.39",
+                 labelStyle: TextStyle(
+                   fontSize: 18,
+                   color: primeColor,
+                 ),
+                 contentPadding: EdgeInsets.fromLTRB(30, 16, 30, 16),
+                 focusColor: primeColor,
+               ),
+               style: TextStyle(
+                   fontSize: 15,
+                   color: primeColor
+               ),
+               controller: _controllerGasolina,
+             ),
+             Padding(
+               padding: EdgeInsets.only(top: 13),
+               child: RaisedButton(
+                 onPressed: () {
+                   print("Dado coletado Álcool: " + _controllerAlcool.text);
+                   print("Dado coletado Gasolina " + _controllerGasolina.text);
+                 },
+                 color: primeColor,
+                 child: Text(
+                   "Calcular",
+                   style: TextStyle(
+                     color: Colors.white,
+                   ),),
+                 padding: EdgeInsets.only(right: 26, left: 26),
+               ),
+             ),
+             Padding(padding: EdgeInsets.only(top: 20),
+             child: Text(
+               "Resultado",
+               textAlign: TextAlign.center,
+               style: TextStyle(
+                 fontSize: 20,
+                 fontWeight: FontWeight.w500,
+                 color: primeColor,
+               ),
+             ),)
+           ],
+         ),
+       ),
       ),
     );
   }
