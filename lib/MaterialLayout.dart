78ce5cd02c87a205c6a1eb50f56805e0c200b9ef
GuiQRSM <endbeob15@gmail.com>
2021-11-24 14:20:19 -0300

    Changes the entire structure of the body: attribute inside Scaffold().

--- lib/MaterialLayout.dart
+++ lib/MaterialLayout.dart
@@ -22,47 +22,7 @@ class _MaterialLayoutState extends State<MaterialLayout> {
           fontWeight: FontWeight.w600
         ),
       ),
-      body: Container(
-        decoration: BoxDecoration(border: Border.all(width: 4, color: Colors.blue),),
-        padding: EdgeInsets.all(16),
-        width: double.infinity,
-        child: Column(
-          children: <Widget>[
-            Image.asset(
-                "logo/newlogocar.png",
-              height: 80,
-            ),
-            Padding(padding: EdgeInsets.only(top:10),),
-            Text("Saiba qual é a melhor opção de abastecimento para o seu carro.",
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.w600,
-              color: Colors.black54,
-            ),),
-            Padding(padding: EdgeInsets.only(top:10),),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: "Preço do Álcool, ex: 1.39",
-                labelStyle: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  color: primeColor
-                ),
-              ),
-            ),
-            TextField(
-             keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: "Preço da Gasolina, ex: 2.40",
-                labelStyle: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  color: primeColor,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
+      body: ,
     );
   }
 }
