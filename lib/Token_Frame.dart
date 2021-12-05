dc0d46a5ddc1798552027bafd78f1fd819517dce
GuiQRSM <endbeob15@gmail.com>
2021-12-05 14:29:04 -0300

    Implement the 'Token_Frame' dart file in the lib folder, importing the dart material and configuring the stful structure which returns the Scaffold() class and brings the 'TokenFrame' class and contains the variable primeColor to set colors.

---
+++ lib/Token_Frame.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class TokenFrame extends StatefulWidget {
+  const TokenFrame({Key? key}) : super(key: key);
+
+  @override
+  _TokenFrameState createState() => _TokenFrameState();
+}
+
+class _TokenFrameState extends State<TokenFrame> {
+
+  var primeColor = Color.fromARGB(255, 168, 26, 29);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
