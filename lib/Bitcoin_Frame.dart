e278610346ae2e924d08dbd51eeb65718f8f2a20
GuiQRSM <endbeob15@gmail.com>
2021-12-14 11:56:35 -0300

    Implement in the 'lib' folder the 'Bitcoin_Frame' dart file with the 'material.dart' import already configured and the 'stful' structure already called

---
+++ lib/Bitcoin_Frame.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BitcoinFrame extends StatefulWidget {
+  const BitcoinFrame({Key? key}) : super(key: key);
+
+  @override
+  _BitcoinFrameState createState() => _BitcoinFrameState();
+}
+
+class _BitcoinFrameState extends State<BitcoinFrame> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
