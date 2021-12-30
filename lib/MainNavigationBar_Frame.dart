5271ed2ac0f63b618cdc04dfe2f4ca8de528835f
GuiQRSM <endbeob15@gmail.com>
2021-12-30 12:19:01 -0300

    Create in the 'lib' folder a new dart file called 'MainNavigationBar_Frame' with the import 'material.dart' and the structure 'stful' returning a class 'Scaffold()' already implemented

---
+++ lib/MainNavigationBar_Frame.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MainNaviagtion extends StatefulWidget {
+  const MainNaviagtion({Key? key}) : super(key: key);
+
+  @override
+  _MainNaviagtionState createState() => _MainNaviagtionState();
+}
+
+var primeColor = Color.fromARGB(255, 168, 26, 29);
+
+class _MainNaviagtionState extends State<MainNaviagtion> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
