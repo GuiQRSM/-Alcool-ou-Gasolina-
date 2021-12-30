3e097b4a34fc0ff54a1ecdc00b2ecfe6938d1054
GuiQRSM <endbeob15@gmail.com>
2021-12-30 12:08:36 -0300

    Creates in the directory 'FramesNaviagtionBar' the dart file 'Chat_Frames', with impotation 'material.dart' and the class 'Scaffold()' being returned by the structure 'stful'

---
+++ lib/FramesNavigationBar/Chat_Frame.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ChatFrame extends StatefulWidget {
+  const ChatFrame({Key? key}) : super(key: key);
+
+  @override
+  _ChatFrameState createState() => _ChatFrameState();
+}
+
+var primeColor = Color.fromARGB(255, 168, 26, 29);
+
+class _ChatFrameState extends State<ChatFrame> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
