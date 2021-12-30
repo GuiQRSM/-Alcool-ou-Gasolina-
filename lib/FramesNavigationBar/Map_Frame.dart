e24b57c257dc346b3b9d1ab636b4bbbc9d81fcab
GuiQRSM <endbeob15@gmail.com>
2021-12-30 12:04:43 -0300

    Create in the 'lib' folder the 'FramesNaviagtionBar' directory by moving the 'MaterialLayout' dart file into it and implementing the 'Map_Frame' dart file with the 'material.dart' impotation and the 'Scaffold()' class being returned by the 'structure' stful'

---
+++ lib/FramesNavigationBar/Map_Frame.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MapFrame extends StatefulWidget {
+  const MapFrame({Key? key}) : super(key: key);
+
+  @override
+  _MapFrameState createState() => _MapFrameState();
+}
+
+var primeColor = Color.fromARGB(255, 168, 26, 29);
+
+class _MapFrameState extends State<MapFrame> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:alcool_ou_gasolina/MaterialLayout_Content/MaterialLayout.dart';
+import 'package:alcool_ou_gasolina/FramesNavigationBar/MaterialLayout.dart';
 import 'package:alcool_ou_gasolina/MaterialLayout_Content/Token_Frame.dart';
 import 'package:flutter/material.dart';
 
