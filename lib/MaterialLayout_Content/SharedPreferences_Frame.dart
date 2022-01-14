ff2b0b149d6989025ce8557d867ef9501dfb2ac7
GuiQRSM <endbeob15@gmail.com>
2022-01-14 13:17:03 -0300

    Implement within the 'MaterialLayout' folder the dart file 'SharedPreferences_Frame' with the import 'material.dart' and the structure 'stful' already called

---
+++ lib/MaterialLayout_Content/SharedPreferences_Frame.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Shared extends StatefulWidget {
+  const Shared({Key? key}) : super(key: key);
+
+  @override
+  _SharedState createState() => _SharedState();
+}
+
+class _SharedState extends State<Shared> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
